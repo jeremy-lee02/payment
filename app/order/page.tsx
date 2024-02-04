5e1cee9d9fd75e2338c976c93974170cfdbabe96
trung tin Le <letrungtin2712@gmail.com>
2024-02-04 14:06:43 +0700

    Update amount format

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -10,6 +10,13 @@ export default function Order (){
         if(statusCode === '00') return "Success"
         return "Failed"
     }
+    function formatAmount (amount: string | null) {
+        if(amount === null) return ""
+        const formattedAmount = amount.slice(0,-2)
+        const formatter = new Intl.NumberFormat('en-US')
+        const result: string = formatter.format(parseFloat(formattedAmount)) 
+        return result + ".00"
+    }
     
     return (
         <Suspense fallback={<>Loading...</>}>
@@ -17,7 +24,7 @@ export default function Order (){
                 <h1 className='font-bold text-3xl'>Payment Status</h1>
                 <div className='flex flex-col items-start justify-center'>
                     <div>
-                    <p className='font-bold'>Amount: <span className=' font-normal'>{param.get('vnp_Amount')}</span></p> 
+                    <p className='font-bold'>Amount: <span className=' font-normal'>{formatAmount(param.get('vnp_Amount'))}</span></p> 
                     </div>
                     <div>
                         <p className='font-bold'>Description: <span className=' font-normal'>{param.get('vnp_OrderInfo')}</span></p> 
