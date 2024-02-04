56d835a2cf781531484388d54144f15e0bc50669
trung tin Le <letrungtin2712@gmail.com>
2024-02-04 13:43:59 +0700

    Add Suspense

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 type Props = {}
 
@@ -14,24 +14,27 @@ export default function Order (){
     return (
         <div className="flex text-xl gap-4 h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
             <h1 className='font-bold text-3xl'>Payment Status</h1>
-            <div className='flex flex-col items-start justify-center'>
-                <div>
-                <p className='font-bold'>Amount: <span className=' font-normal'>{param.get('vnp_Amount')}</span></p> 
-                </div>
-                <div>
-                    <p className='font-bold'>Description: <span className=' font-normal'>{param.get('vnp_OrderInfo')}</span></p> 
-                </div>
-                <div>
-                    <p className='font-bold'>Bank: <span className=' font-normal'>{param.get('vnp_BankCode')}</span></p> 
-                </div>
-                <div>
-                    <p className='font-bold'>Code: <span className=' font-normal'>{param.get('vnp_TxnRef')}</span></p> 
-                </div>
-                <div>
-                    <p className='font-bold'>Status: <span className=' font-normal'>{checkStatus(param.get('vnp_TransactionStatus'))}</span></p> 
+            <Suspense fallback={<>Loading...</>}>
+                <div className='flex flex-col items-start justify-center'>
+                    <div>
+                    <p className='font-bold'>Amount: <span className=' font-normal'>{param.get('vnp_Amount')}</span></p> 
+                    </div>
+                    <div>
+                        <p className='font-bold'>Description: <span className=' font-normal'>{param.get('vnp_OrderInfo')}</span></p> 
+                    </div>
+                    <div>
+                        <p className='font-bold'>Bank: <span className=' font-normal'>{param.get('vnp_BankCode')}</span></p> 
+                    </div>
+                    <div>
+                        <p className='font-bold'>Code: <span className=' font-normal'>{param.get('vnp_TxnRef')}</span></p> 
+                    </div>
+                    <div>
+                        <p className='font-bold'>Status: <span className=' font-normal'>{checkStatus(param.get('vnp_TransactionStatus'))}</span></p> 
+                    </div>
+
                 </div>
 
-            </div>
+            </Suspense>
             
 
         </div>
